dcca6e0ecc5d12dd9ab49a6336dd3b90b96f1ba1
mbufi <mbufi1992@gmail.com>
2018-06-03 12:43:11 -0400

    Update wall_follower.sh

--- ShellScripts/wall_follower.sh
+++ ShellScripts/wall_follower.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+##################################################################
+#
+# Udacity Term 2 Home Service Project launch script
+#          Martin Bufi June 2018
+#
+# Automate the process to let the robot follow the walls and 
+# autonomously map the environment while avoiding obstacles. 
+#
+################################################################
+
 xterm  -e  "export TURTLEBOT_GAZEBO_WORLD_FILE=~/catkin_ws/src/World/house.world; source ~/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm  -e  "source ~/catkin_ws/devel/setup.bash; roslaunch home_services_robot gmapping.launch" &
