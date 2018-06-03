33fc3b2ace4c9ae6496280e181bf27aaf69f4bd6
mbufi <mbufi1992@gmail.com>
2018-06-03 12:42:42 -0400

    Update test_slam.sh

--- ShellScripts/test_slam.sh
+++ ShellScripts/test_slam.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+##################################################################
+#
+# Udacity Term 2 Home Service Project launch script
+#          Martin Bufi June 2018
+#
+# Test SLAM operation by using interactive markers or 
+# teleop_keyboard to create a map.
+#
+################################################################
+
 xterm  -e  "export TURTLEBOT_GAZEBO_WORLD_FILE=~/catkin_ws/src/World/house.world; source ~/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm  -e  "source ~/catkin_ws/devel/setup.bash; roslaunch home_services_robot gmapping.launch" &
