1a471267727784490f35f6b9e8cbccc2496abf43
mbufi <mbufi1992@gmail.com>
2018-06-03 12:41:16 -0400

    Update home_service.sh

--- ShellScripts/home_service.sh
+++ ShellScripts/home_service.sh
@@ -1,4 +1,17 @@
-
+##################################################################
+#
+# Udacity Term 2 Home Service Project launch script
+#          Martin Bufi June 2018
+#
+# Navigate to two goals and carry a marker from the pickup goal to
+# the dropoff goal.
+# The first goal should be your desired pickup goal and the second goal
+# should be your desired drop off goal. The robot has to travel to the
+# desired pickup zone, display a message that it reached its destination,
+# wait 5 seconds, travel to the desired drop off zone, and display a 
+# message that it reached the drop off zone.
+#
+################################################################
 xterm  -e  "export TURTLEBOT_GAZEBO_WORLD_FILE=~/catkin_ws/src/World/house.world; source ~/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm  -e  "source ~/catkin_ws/devel/setup.bash; roslaunch home_services_robot amcl.launch" &
