a3eef1608d8d19717596c0149fb195d353943798
mbufi <mbufi1992@gmail.com>
2018-06-03 12:42:12 -0400

    Update test_navigation.sh

--- ShellScripts/test_navigation.sh
+++ ShellScripts/test_navigation.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+##################################################################
+#
+# Udacity Term 2 Home Service Project launch script
+#          Martin Bufi June 2018
+#
+# Manually point out to two different goals, one at a time, 
+# and direct your robot to reach them and orient itself with 
+# respect to them.
+################################################################
 xterm  -e  "export TURTLEBOT_GAZEBO_WORLD_FILE=~/catkin_ws/src/World/house.world; source ~/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm  -e  "source ~/catkin_ws/devel/setup.bash; roslaunch home_services_robot amcl.launch" &
